3d3a720cbce545732027e33669eef2309841a9b4
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-16 12:38:00 +0900

    feat: TCP 연결 방식

---
+++ CrazyArcade/Client/TcpClient.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hSocket;
+    SOCKADDR_IN servAddr;
+
+    char message[30];
+    int strLen = 0;
+    int idx = 0, readLen = 0;
+
+    if (argc != 3)
+    {
+        printf("Usage: %s <IP> <PORT>\n", argv[0]);
+        exit(0);
+    }
+
+    // 소켓 라이브러리 초기화
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성, TCP 소켓 생성
+    hSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (hSocket == INVALID_SOCKET)
+    {
+        ErrorHandling("socket() error");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &servAddr.sin_addr.s_addr);
+    servAddr.sin_port = htons(atoi(argv[2]));
+
+    // 생성된 소켓을 바탕으로 서버에 연결 요청
+    if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    {
+        ErrorHandling("connect() error");
+    }
+
+    // while문 안에서 recv 함수를 통해, 수신된 데이터를 1바이트씩 읽는다
+    while (readLen = recv(hSocket, &message[idx++], 1, 0))
+    {
+        if (readLen == -1)
+        {
+            ErrorHandling("read() error");
+        }
+
+        // 1바이트씩 데이터를 읽고 있기 때문에 strLen에 1씩 더해지며, 이는 recv 함수의 호출 횟 수와 같다.
+        strLen += readLen;
+    }
+
+    printf("Message from server: %s \n", message);
+    printf("Function read call count: %d \n", strLen);
+
+    closesocket(hSocket);
+
+    // 소켓 라이브러리 해제
+    WSACleanup();
+
+    return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ CrazyArcade/Server/TcpServer.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+    // 초기화되는 라이브러리의 정보 변수
+    WSADATA wsaData;
+    // 소켓 변수
+    SOCKET hServsock, hClntSock;
+    SOCKADDR_IN servAddr, clntAddr;
+
+    int szClntAddr;
+    char message[] = "Hello World!";
+    if (argc != 2)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    // 소켓 라이브러리 초기화 함수
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성
+    hServsock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServsock == INVALID_SOCKET)
+    {
+        ErrorHandling("socket() error");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi(argv[1]));
+
+    // 소켓에 IP 주소와 PORT 번호 할당
+    if (bind(hServsock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    {
+        ErrorHandling("bind() error");
+    }
+
+    // listen 함수를 통해 생성한 소켓을 서버 소켓으로 완성
+    if (listen(hServsock, 5) == SOCKET_ERROR)
+    {
+        ErrorHandling("listen() error");
+    }
+
+    // 클라이언트의 연결 요청을 수락하기 위해서 accept 함수 호출
+    szClntAddr = sizeof(clntAddr);
+    hClntSock = accept(hServsock, (SOCKADDR*)&clntAddr, &szClntAddr);
+    if (hClntSock == INVALID_SOCKET)
+    {
+        ErrorHandling("accept() error");
+    }
+
+    // send 함수 호출을 통해서 연결된 클라이언트에 데이터 전송
+    send(hClntSock, message, sizeof(message), 0);
+    closesocket(hClntSock);
+    closesocket(hServsock);
+
+    // 초기화한 소켓 라이브러리 반환 해제 및 이후 왼속 관련 함수 호출 불가
+    WSACleanup();
+
+    return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
