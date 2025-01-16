3f669769999fe64774bfd43085ce0517dbd1def8
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-16 16:03:20 +0900

    feat: 계산기 서버

---
+++ CrazyArcade/Client/CalcClient.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+#define BUF_SIZE  1024
+#define RLT_SIZE 4
+#define OPSZ 4
+#pragma comment (lib, "Ws2_32.lib")
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hSocket;
+    SOCKADDR_IN servAddr;
+
+    char opmsg[BUF_SIZE];
+    int result, opndCnt, i;
+
+    if (argc != 3)
+    {
+        printf("Usage: %s <IP> <PORT>\n", argv[0]);
+        exit(0);
+    }
+
+    // 소켓 라이브러리 초기화
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성, TCP 소켓 생성
+    hSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (hSocket == INVALID_SOCKET)
+    {
+        ErrorHandling("socket() error");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &servAddr.sin_addr.s_addr);
+    servAddr.sin_port = htons(atoi(argv[2]));
+
+    // 생성된 소켓을 바탕으로 서버에 연결 요청
+    if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    {
+        ErrorHandling("connect() error");
+    }
+    else
+    {
+        puts("Connected.........");
+    }
+
+    fputs("Operand count: ", stdout);
+    scanf_s("%d", &opndCnt);
+    opmsg[0] = (char)opndCnt;
+
+    for (int i = 0; i < opndCnt; ++i)
+    {
+        printf("Operand %d: ", i + 1);
+        scanf_s("%d", (int*)&opmsg[i * OPSZ + 1]);
+    }
+    fgetc(stdin);
+    fputs("Operator: ", stdout);
+    scanf_s("%c", &opmsg[opndCnt * OPSZ + 1], 1);
+    send(hSocket, opmsg, opndCnt * OPSZ + 1 + 1, 0);
+
+    recv(hSocket, reinterpret_cast<char*>(&result), RLT_SIZE, 0);
+
+    printf("Operation result: %d \n", result);
+
+    closesocket(hSocket);
+
+    // 소켓 라이브러리 해제
+    WSACleanup();
+
+    return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ CrazyArcade/Server/CalcServer.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+#define BUF_SIZE 1024
+#define OPSZ 4
+void ErrorHandling(const char* message);
+int Calculate(int opnum, int opnds[], char op);
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hServSock, hClntSock;
+    char opinfo[BUF_SIZE] = {0};
+    int result, opndCnt;
+    int recvCnt, recvLen;
+
+    SOCKADDR_IN servAddr, clntAddr;
+    int clntAddrSize;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <port>\n ", argv[0]);
+        exit(1);
+    }
+
+    // 소켓 라이브러리 초기화 함수
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == INVALID_SOCKET)
+    {
+        ErrorHandling("socket() error");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi(argv[1]));
+
+    // 소켓에 IP 주소와 PORT 번호 할당
+    if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    {
+        ErrorHandling("bind() error");
+    }
+
+    // listen 함수를 통해 생성한 소켓을 서버 소켓으로 완성
+    if (listen(hServSock, 5) == SOCKET_ERROR)
+    {
+        ErrorHandling("listen() error");
+    }
+
+
+    // 클라이언트의 연결 요청을 수락하기 위해서 accept 함수 호출
+    clntAddrSize = sizeof(clntAddr);
+
+    for (int i = 0; i < 5; i++)
+    {
+        opndCnt = 0;
+        hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &clntAddrSize);
+        recv(hClntSock, reinterpret_cast<char*>(&opndCnt), 1, 0);
+
+        recvLen = 0;
+        while ((opndCnt * OPSZ + 1) > recvLen)
+        {
+            recvCnt = recv(hClntSock, &opinfo[recvLen], BUF_SIZE - 1, 0);
+
+            if (recvCnt >= 0)
+            {
+                recvLen += recvCnt;
+            }
+        }
+
+        int opnds[256];
+        for (int i = 0; i < opndCnt; ++i) 
+        {
+            memcpy(&opnds[i], &opinfo[i * 4], 4);
+        }
+        char op = opinfo[opndCnt * 4];
+
+        result = Calculate(opndCnt, opnds, op);
+        send(hClntSock, (char*)&result, sizeof(result), 0);
+
+        closesocket(hClntSock);
+    }
+
+    closesocket(hServSock);
+
+    // 초기화한 소켓 라이브러리 반환 해제 및 이후 왼속 관련 함수 호출 불가
+    WSACleanup();
+
+    return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int Calculate(int opnum, int opnds[], char op)
+{
+    int res = opnds[0];
+
+    switch (op)
+    {
+    case '+':
+        for (int i = 0; i < opnum; ++i)
+        {
+            res += opnds[i];
+        }
+        break;
+    case '-':
+        for (int i = 0; i < opnum; ++i)
+        {
+            res -= opnds[i];
+        }
+        break;
+    case '*':
+        for (int i = 0; i < opnum; ++i)
+        {
+            res *= opnds[i];
+        }
+        break;
+    }
+
+    return res;
+}
