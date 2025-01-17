af05f082f23ed51235e32f0d2bac7089b1c6b4fb
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-17 15:01:57 +0900

    feat: 멀티스레드와 뮤텍스를 사용해 채팅서버구현

---
+++ CrazyArcade/Client/ChatClient.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <Windows.h>
+#include <process.h>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+#define BUF_SIZE  100
+#define NAME_SIZE 20
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+#define Test 1
+
+unsigned WINAPI SendMsg(void* arg);
+unsigned WINAPI RecvMsg(void* arg);
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hSocket;
+    SOCKADDR_IN servAddr;
+    HANDLE hSndThread, hRcvThread;
+
+#if !Test
+    if (argc != 4)
+    {
+        printf("Usage: %s <IP> <PORT>\n", argv[0]);
+        exit(0);
+    }
+#endif
+
+    // 소켓 라이브러리 초기화
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성, TCP 소켓 생성
+    hSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (hSocket == INVALID_SOCKET)
+    {
+        ErrorHandling("socket() error");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &servAddr.sin_addr.s_addr);
+    servAddr.sin_port = htons(atoi(argv[2]));
+
+    // 생성된 소켓을 바탕으로 서버에 연결 요청
+    if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    {
+        ErrorHandling("connect() error");
+    }
+    else
+    {
+        puts("Connected.........");
+    }
+
+    hSndThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&hSocket, 0, NULL);
+    hRcvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&hSocket, 0, NULL);
+
+    WaitForSingleObject(hSndThread, INFINITE);
+    WaitForSingleObject(hRcvThread, INFINITE);
+
+    closesocket(hSocket);
+
+    // 소켓 라이브러리 해제
+    WSACleanup();
+
+    return 0;
+}
+
+unsigned WINAPI SendMsg(void* arg) // send thread main
+{
+    SOCKET hSocket = *((SOCKET*)arg);
+    char nameMsg[NAME_SIZE + BUF_SIZE];
+    while (true)
+    {
+        fgets(msg, BUF_SIZE, stdin);
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
+        {
+            closesocket(hSocket);
+            exit(0);
+        }
+        sprintf_s(nameMsg, sizeof(nameMsg), "%s %s", name, msg);
+        send(hSocket, nameMsg, strlen(nameMsg), 0);
+    }
+
+    return 0;
+}
+
+unsigned WINAPI RecvMsg(void* arg) // read thread main
+{
+    SOCKET hSocket = *((SOCKET*)arg);
+    char nameMsg[NAME_SIZE + BUF_SIZE];
+    int strLen;
+    while (true)
+    {
+        strLen = recv(hSocket, nameMsg, NAME_SIZE + BUF_SIZE - 1, 0);
+        if (strLen == -1)
+        {
+            return -1;
+        }
+        nameMsg[strLen] = 0;
+        fputs(nameMsg, stdout);
+    }
+
+    return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
---
+++ CrazyArcade/Server/ChatServer.cpp
@@ -0,0 +1,152 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <Windows.h>
+#include <process.h>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+#define TEST 1
+
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+unsigned WINAPI HandleClnt(void* arg);
+void SendMsg(char* msg, int len);
+void ErrorHandling(const char* message);
+
+int clntCnt = 0;
+SOCKET clntSocks[MAX_CLNT];
+HANDLE hMutex;
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsaData;
+    SOCKET hServSock, hClntSock;
+    SOCKADDR_IN servAddr, clntAddr;
+    int clntAddrSize;
+    HANDLE hThread;
+
+#if !TEST
+    if (argc != 2)
+    {
+        printf("Usage: %s <port>\n ", argv[0]);
+        exit(1);
+    }
+#endif
+
+    // 소켓 라이브러리 초기화 함수
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        ErrorHandling("WSAStartup() error!");
+    }
+
+    // 소켓 생성
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == INVALID_SOCKET)
+    {
+        ErrorHandling("socket() error");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+#if TEST
+    servAddr.sin_port = htons(9190);
+#else
+    servAddr.sin_port = htons(atoi(argv[1]));
+#endif
+
+
+    // 소켓에 IP 주소와 PORT 번호 할당
+    if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+    {
+        ErrorHandling("bind() error");
+    }
+
+    // listen 함수를 통해 생성한 소켓을 서버 소켓으로 완성
+    if (listen(hServSock, 5) == SOCKET_ERROR)
+    {
+        ErrorHandling("listen() error");
+    }
+
+    while (true)
+    {
+        clntAddrSize = sizeof(clntAddr);
+        hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &clntAddrSize);
+
+        WaitForSingleObject(hMutex, INFINITE);
+        clntSocks[clntCnt++] = hClntSock;
+        ReleaseMutex(hMutex);
+
+        hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void*)&hClntSock, 0, NULL);
+        char clientIP[20] = { 0 };
+        if (inet_ntop(AF_INET, &clntAddr.sin_addr, clientIP, sizeof(clientIP)))
+        {
+            printf("Connected client IP: %s \n", clientIP);
+        }
+        else
+        {
+            ErrorHandling("inet_ntop() error");
+        }
+    }
+  
+    closesocket(hServSock);
+
+    // 초기화한 소켓 라이브러리 반환 해제 및 이후 왼속 관련 함수 호출 불가
+    WSACleanup();
+
+    return 0;
+}
+
+unsigned WINAPI HandleClnt(void* arg)
+{
+    SOCKET hClntSock = *((SOCKET*)arg);
+    int strLen = 0;
+    char msg[BUF_SIZE];
+
+    while ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0)
+    {
+        SendMsg(msg, strLen);
+    }
+
+    WaitForSingleObject(hMutex, INFINITE);
+    for (int i = 0; i < clntCnt; i++) // remove disconnected chilent
+    {
+        if (hClntSock == clntSocks[i])
+        {
+            while (i++ < clntCnt - 1)
+            {
+                clntSocks[i] = clntSocks[i + 1];
+
+                break;
+            }
+        }
+    }
+
+
+    clntCnt--;
+    ReleaseMutex(hMutex);
+    closesocket(hClntSock);
+    return 0;
+}
+
+void SendMsg(char* msg, int len) // send to all
+{
+    WaitForSingleObject(hMutex, INFINITE);
+    for (int i = 0; i < clntCnt; i++)
+    {
+        send(clntSocks[i], msg, len, 0);
+    }
+    ReleaseMutex(hMutex);
+}
+
+void ErrorHandling(const char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
