89909133282ff910b975815bb80d42927ab31ac9
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-01-20 15:13:30 +0900

    feat: 패킷 정의

---
+++ CrazyArcade/ConsoleEngine/Network/Packets.cpp
@@ -0,0 +1,13 @@
+﻿#include "PrecompiledHeader.h"
+
+#include "Packets.h"
+
+void ENGINE_API SerializePacket(const void* packet, size_t packetSize, char* buffer)
+{
+    memcpy(buffer, packet, packetSize);
+}
+
+void ENGINE_API DeserializePacket(void* packet, size_t packetSize, const char* buffer)
+{
+    memcpy(packet, buffer, packetSize);
+}
--- CrazyArcade/ConsoleEngine/Network/Packets.h
+++ CrazyArcade/ConsoleEngine/Network/Packets.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <Core.h>
@@ -6,19 +6,54 @@
 enum class ENGINE_API PacketType
 {
     INPUT = 1,
+    MOVE,
 };
 
 struct ENGINE_API PacketHeader 
 {
-    uint16_t packetType;
-    uint16_t packetSize;
+public:
+    uint32_t packetType;
+    uint32_t packetSize;
 };
 
 struct ENGINE_API InputPacket
 {
+public:
     PacketHeader header;
-    uint32_t playerID;
+    uint32_t playerId;
     uint8_t keyCode;
-    uint8_t action;
-    uint32_t timestamp;
+
+public:
+    InputPacket(const PacketType& packetType, const uint32_t& packetSize,
+        const uint32_t& playerId, const uint8_t& keyCode)
+        : playerId(playerId), keyCode(keyCode)
+    {
+        header.packetType = (uint32_t)packetType;
+        header.packetSize = packetSize;
+    }
 };
+
+struct ENGINE_API ActorMovePacket
+{
+public:
+    PacketHeader header;
+    uint32_t movedActorIndex;
+    uint32_t posX;
+    uint32_t posY;
+
+public:
+    ActorMovePacket(const PacketType& packetType, const uint32_t& packetSize,
+        const uint32_t& movedActorIndex, 
+        const uint32_t& posX, const uint32_t& posY)
+        : movedActorIndex(movedActorIndex), posX(posX), posY(posY)
+    {
+        header.packetType = (uint32_t)packetType;
+        header.packetSize = packetSize;
+    }
+};
+
+extern "C"
+{
+    void ENGINE_API SerializePacket(const void* packet, size_t packetSize, char* buffer);
+    void ENGINE_API DeserializePacket(void* packet, size_t packetSize, const char* buffer);
+}
